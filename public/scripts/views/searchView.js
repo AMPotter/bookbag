7076bd84a59622fe6db5868cb279822233a900af
Anwar Montasir <anwarmontasir@gmail.com>
2017-06-20 16:54:09 -0700

    search queries are now compatible with URL structure

---
+++ public/scripts/views/searchView.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var app = app || {};
+
+(function(module) {
+    const searchView = {};
+
+    searchView.initSearch = function() {
+        $('#search-form').on('submit', searchView.submit);
+    }
+
+    searchView.submit = function(event) {
+        event.preventDefault();
+        // replace spaces with plus signs for URL
+        const titleString = $('#search-title').val().split(' ').join('+');
+        console.log(titleString);
+        // let searchQuery = new app.Result({
+        //     Name: $('#search-title').val()
+        // });
+        //app.Result.fetchAll(app.resultsView.index);
+    }
+
+    searchView.initSearch();
+    module.searchView = searchView;
+})(app);
