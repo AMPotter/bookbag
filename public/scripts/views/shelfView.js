a31c33675c8a95bd5821f7e37a1e7357188bb39c
Haley Tortorella <hdtort@gmail.com>
2017-06-22 18:08:07 -0700

    styled and fixed minor issues

--- public/scripts/views/shelfView.js
+++ public/scripts/views/shelfView.js
@@ -2,22 +2,25 @@
 
 var app = app || {};
 
-(function(module) {
+(function (module) {
     const shelfView = {};
 
-    const render = function(result) {
+    const render = function (result) {
         let template = Handlebars.compile($('#shelf-template').text());
         return template(result);
     };
 
-    shelfView.initPage = function() {
+    shelfView.initPage = function () {
         $('#shelf').show().siblings().hide();
         $('#shelf article').remove();
+        $('#shelf-header').show();
         if (app.Shelf.all.length === 0) {
-            $('no-shelf').show();
+            $('#no-shelf').show();
+            $('#shelf-header').hide();
         } else {
             app.Shelf.all.forEach(a => $('#shelf').append(render(a)));
             app.shelfView.killFave();
+            $('#no-shelf').hide();
         }
     };
     shelfView.killFave = function () {
@@ -25,7 +28,13 @@ var app = app || {};
             var bookId = $(this).attr('data-book-id');
             //var arrayPos = app.Shelf.all[index];
             app.Shelf.deleteRecord(bookId);
-            $(this).parent().parent().slideUp();
+            $(this).parent().parent().slideUp(300, function () {
+                // console.log(app.Shelf.all.length);
+                // if (app.Shelf.all.length === 0) {
+                //     $('#no-shelf').show();
+                //     $('#shelf-header').hide();
+                // }
+            });
         });
     };
 
