2d365ecad6177a00e56fb6a0318574cc5b12fa4c
Anwar Montasir <anwarmontasir@gmail.com>
2017-06-23 10:01:34 -0700

    remove-logs

--- public/scripts/views/shelfView.js
+++ public/scripts/views/shelfView.js
@@ -26,15 +26,8 @@ var app = app || {};
     shelfView.killFave = function () {
         $('.kill-fave').on('click', function (event) {
             var bookId = $(this).attr('data-book-id');
-            //var arrayPos = app.Shelf.all[index];
             app.Shelf.deleteRecord(bookId);
-            $(this).parent().parent().slideUp(300, function () {
-                // console.log(app.Shelf.all.length);
-                // if (app.Shelf.all.length === 0) {
-                //     $('#no-shelf').show();
-                //     $('#shelf-header').hide();
-                // }
-            });
+            $(this).parent().parent().slideUp();
         });
     };
 
